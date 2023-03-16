a70b7cc6bf94c61f3b961a4077fe017b103f9caf
Shreshth Sharma <shresthrw@gmail.com>
2023-03-16 18:25:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def main():
             cut = img.shape[0]//6
             for i in range(6):
                 for j in range(6):
-                    r1 = int(output[i*6 + j])
+                    r1 = int(output[0][i*6 + j])
                     r = int(r1/6)
                     c = int(r1%6)
                     new_im[r*cut:(r+1)*cut, c*cut:(c+1)*cut] = img[i*cut:(i+1)*cut, j*cut:(j+1)*cut]
